a40090fbf12d40b7833a58ab7d904c985cdc5548
sagar9crack <iamsagar09@gmail.com>
2020-10-11 10:07:33 +0530

    Update speechEmotionRecognition.py

--- src/speechEmotionRecognition.py
+++ src/speechEmotionRecognition.py
@@ -74,3 +74,52 @@ if __name__ == '__main__':
                 writer.writerow(csvData)
         csvFile.close()
     os.remove("filterTemp.wav")
+                                        
+                                        
+mylist= os.listdir('INPUT_FOLDER_PATH')
+ type(mylist)
+ print(mylist[1800])  
+ print(mylist[400][6:-16])
+ 
+data, sampling_rate = librosa.load('INPUT_FOLDER_PATH')
+% pylab inline
+import os
+import pandas as pd
+import librosa
+import glob 
+
+plt.figure(figsize=(15, 5))
+librosa.display.waveplot(data, sr=sampling_rate)
+
+import matplotlib.pyplot as plt
+import scipy.io.wavfile
+import numpy as np
+import sys
+
+
+sr,x = scipy.io.wavfile.read('INPUT_FOLDER_PATH')
+
+## Parameters: 10ms step, 30ms window
+nstep = int(sr * 0.01)
+nwin  = int(sr * 0.03)
+nfft = nwin
+
+window = np.hamming(nwin)
+
+## will take windows x[n1:n2].  generate
+## and loop over n2 such that all frames
+## fit within the waveform
+nn = range(nwin, len(x), nstep)
+
+X = np.zeros( (len(nn), nfft//2) )
+
+for i,n in enumerate(nn):
+    xseg = x[n-nwin:n]
+    z = np.fft.fft(window * xseg, nfft)
+    X[i,:] = np.log(np.abs(z[:nfft//2]))
+
+plt.imshow(X.T, interpolation='nearest',
+    origin='lower',
+    aspect='auto')
+
+plt.show()
